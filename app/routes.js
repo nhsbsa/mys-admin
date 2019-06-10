6d16adc2cfeb2f658638e5b0fc26d65751ed843b
Stephen Hill <stephen.hill9@nhs.net>
2019-06-10 14:12:02 +0100

    Added management dashboard

--- app/routes.js
+++ app/routes.js
@@ -9,3 +9,29 @@ router.get('/', function (req, res) {
 // add your routes here
 
 module.exports = router
+
+// REPORTS DOWNLOAD
+
+router.get(/downloadreports-handler/, function (req, res) {
+  if (req.query.reports.includes('fp34c')) {
+    res.redirect('/public/images/bsa/MYS Report - FP34c.xlsx');
+  } else if (req.query.reports.includes('returns')) {
+    res.redirect('/public/images/bsa/MYS Report - Prescription Returns.xlsx');
+  } else if (req.query.reports.includes('flu')) {
+    res.redirect('/public/images/bsa/MYS Report - Flu Submissions.xlsx');
+  } else if (req.query.reports.includes('numsas')) {
+    res.redirect('reports3');
+  } else {
+    res.redirect('reports3');
+  }
+});
+
+router.get(/reportrange-handler/, function (req, res) {
+  if (req.query.reportrange.includes('singlemonth')) {
+    res.redirect('month');
+  } else if (req.query.reportrange.includes('monthrange')) {
+    res.redirect('reports-range');
+  } else {
+    res.redirect('reports2');
+  }
+});
