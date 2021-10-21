8f9b21259307e8c923924dae2d814c3074a565e5
Douglas Caven <douglas.caven@nhs.net>
2021-10-21 09:33:51 +0100

    Doug

--- app/routes.js
+++ app/routes.js
@@ -651,7 +651,7 @@ router.get(/pqsGOne/, function (req, res) {
 // PQS 20/21 //
 router.get(/pqsAddP/, function (req, res) {
   if (req.query.radioInlineGroup === "Yes" ) {
-    res.redirect('pqs-pcn-lead-journey-1'); // if both input values are "Yes" - redirect to 'page-name' //
+    res.redirect('pqs-pcn-lead-journey-11'); // if both input values are "Yes" - redirect to 'page-name' //
   } else if (req.query.radioInlineGroup === "No" ){
     res.redirect('pqs-pcn-lead-journey-cya'); // if both input values are "Yes" - redirect to 'page-name' //
   }
@@ -660,7 +660,7 @@ router.get(/pqsAddP/, function (req, res) {
 // PQS 20/21 //
 router.get(/pqsAddpharm/, function (req, res) {
   if (req.query.radioInlineGroup === "Yes" ) {
-    res.redirect('pqs-non-pcn-lead-journey-1'); // if both input values are "Yes" - redirect to 'page-name' //
+    res.redirect('pqs-non-pcn-lead-journey-11'); // if both input values are "Yes" - redirect to 'page-name' //
   } else if (req.query.radioInlineGroup === "No" ){
     res.redirect('pqs-non-pcn-lead-journey-cya'); // if both input values are "Yes" - redirect to 'page-name' //
   }
